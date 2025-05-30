05307646e2ce3ee71228c4f0d819ad584e085b90
Sascha Haeberling <mail@sascha-haeberling.com>
2025-05-29 15:52:16 -1000

    Specify the service name in the push script.

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcloud run deploy --source . --service-account=pg-service-account@pantryguardian-f8381.iam.gserviceaccount.com --max-instances=1 --port=5000
+gcloud run deploy expiration-reminder --source . --service-account=pg-service-account@pantryguardian-f8381.iam.gserviceaccount.com --max-instances=1 --port=5000
